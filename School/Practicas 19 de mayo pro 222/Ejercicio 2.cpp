6d152b3b98ac9c45ea3ded58e5d44b19a0bc75ef
TheGabloxia <70188611+TG609@users.noreply.github.com>
2022-05-19 21:34:59 -0500

    Delete School directory

--- School/Practicas 19 de mayo pro 222/Ejercicio 2.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> /* la función main inicia la ejecución del programa */
-int main (void)
-{
-	printf("Bienvenido a C!\n");
-	return 0; /* indica que el programa terminó con exito */
-} /* Fin de la funcion main */
-
-
--- School/Practicas 19 de mayo pro 222/Practica 4.cpp
+++
@@ -1,9 +0,0 @@
-/* PRactica 4 */
-/* Gabriel Pro 222 */
-#include <stdio.h>
-/* Para utilizar: printf(), getchar() */
-main ()
-{
-	int numero1, numero2;
-	int suma,resta,multiplicacion,division,modulo;
-}
